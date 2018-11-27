bbbac17ef0eb4b9e20438c4169fe76a3eb19937e
mhisaki028 <45382111+mhisaki028@users.noreply.github.com>
2018-11-27 15:04:21 +0800

    Update AppInfoFragment.java

--- app/src/main/java/imn/dev/androidpatientapp/AppInfoFragment.java
+++ app/src/main/java/imn/dev/androidpatientapp/AppInfoFragment.java
@@ -14,6 +14,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class AppInfoFragment extends Fragment {
 
+//////HELLO
 
     String[] listviewTitle = new String[]{
             "Appointments", "Reminders", "Lab Test Results",
